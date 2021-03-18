1f762f444a7a6f427a1f7061e1f79ac947def581
valentinscarlatescu <valentin.scarlatescu98@gmail.com>
2021-03-18 22:12:32 +0200

    Created the Product table

---
+++ src/main/java/com/aslbackend/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.aslbackend.controller;
+
+import com.aslbackend.data.model.Product;
+import com.aslbackend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class ProductController {
+
+    private final ProductService service;
+
+    @Autowired
+    ProductController (ProductService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Product> findAll(){
+        return service.findAll();
+    }
+}
--- src/main/java/com/aslbackend/data/model/ProductCategory.java
+++ src/main/java/com/aslbackend/data/model/ProductCategory.java
@@ -1,11 +1,14 @@
 package com.aslbackend.data.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class ProductCategory {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     private String name;
 
