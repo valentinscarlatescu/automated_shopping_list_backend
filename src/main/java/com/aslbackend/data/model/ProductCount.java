84d26f063cdd88a6733bf5683e82d3805696d7c0
valentinscarlatescu <valentin.scarlatescu98@gmail.com>
2021-05-12 23:30:53 +0300

    Add home recommendations

---
+++ src/main/java/com/aslbackend/data/model/ProductCount.java
@@ -0,0 +1,28 @@
+package com.aslbackend.data.model;
+
+public class ProductCount {
+    private Product product;
+    private Long count;
+
+    public ProductCount(Product product, Long count) {
+        this.product = product;
+        this.count = count;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+}
